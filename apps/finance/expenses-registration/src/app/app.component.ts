1160fbc2a33b3982d27d1e410e3ebb055ff5d069
Your Name <you@example.com>
2024-08-30 09:22:48 +0200

    Navbar component

--- apps/finance/expenses-registration/src/app/app.component.ts
+++ apps/finance/expenses-registration/src/app/app.component.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { NavbarComponent, NavbarItem } from '@bt-libs/shared/ui/common-components';
 
 
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, NavbarComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'finance-expenses-registration';
+
+  navItems: NavbarItem[] = [
+    {label: 'home', route: '/'},
+    {label: 'expenses overview', route: '/expenses-overview'},
+    {label: 'expenses approval', route: '/expenses-approval'},
+  ];
+
+  languages: string[] = [
+    'fr',
+    'de',
+    'gb'
+  ]
+
 }
---
+++ libs/shared/ui/common-components/lib/navbar/navbar.component.ts
@@ -0,0 +1,39 @@
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'bt-libs-ui-navbar',
+  standalone: true,
+  imports: [CommonModule, RouterLink],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class NavbarComponent implements OnChanges, OnInit {
+
+
+  @Input() navbarItems!: NavbarItem[];
+  // navbarItems = input([], { transform: addHome });
+  @Input() languages!: string[];
+  // languages = input<string[]>([]);
+
+  @Output() languageChange = new EventEmitter();
+
+  ngOnInit(): void {
+    console.log(' init:')
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(' chgna:', changes)
+  }
+}
+
+function addHome(items: NavbarItem[]): NavbarItem[] {
+  return [{ label: 'home', route: '/' }, ...items];
+}
+
+export interface NavbarItem {
+  label: string;
+  route: string
+}
