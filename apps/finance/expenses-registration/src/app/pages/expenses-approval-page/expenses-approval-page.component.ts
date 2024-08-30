c75716134a9e6f8aaae27f04a76bca2be8a79b09
Your Name <you@example.com>
2024-08-30 10:11:27 +0200

    output simple binding

--- apps/finance/expenses-registration/src/app/pages/expenses-approval-page/expenses-approval-page.component.ts
+++ apps/finance/expenses-registration/src/app/pages/expenses-approval-page/expenses-approval-page.component.ts
@@ -1,12 +1,19 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TableComponent } from '@bt-libs/shared/ui/common-components';
 
 @Component({
   selector: 'app-expenses-approval-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TableComponent],
   templateUrl: './expenses-approval-page.component.html',
   styleUrl: './expenses-approval-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class ExpensesApprovalPageComponent {}
+export default class ExpensesApprovalPageComponent {
+
+  handleDataChange($event: any) {
+    console.log(' change $event', $event)
+  }
+
+}
--- libs/shared/ui/common-components/src/index.ts
+++ libs/shared/ui/common-components/src/index.ts
@@ -1,1 +1,2 @@
 export * from '../lib/navbar/navbar.component'
+export * from '../lib/table/table.component'
