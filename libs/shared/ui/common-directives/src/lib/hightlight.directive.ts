e98fb7ec3f3c74dba2e4cef542ae6e2d3ea70318
Your Name <you@example.com>
2024-09-06 09:51:02 +0200

    HostListener

--- libs/shared/ui/common-directives/src/lib/hightlight.directive.ts
+++ libs/shared/ui/common-directives/src/lib/hightlight.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, HostBinding, inject, OnInit } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, inject, Input, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[btLibsUiHightlight]',
@@ -6,7 +6,28 @@ import { Directive, ElementRef, HostBinding, inject, OnInit } from '@angular/cor
 })
 export class HightlightDirective implements OnInit {
   
-  @HostBinding('style.backgroundColor') get Color() { return 'red'}
+  //@HostBinding('style.backgroundColor') get Color() { return 'red'}
+
+  @Input('btLibsUiHightlight') background!: string;
+
+  @Input() textColor!: string;
+
+  originalColor = 'black';
+  originalBackground = 'white';
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.originalColor = this.el.style.color;
+    this.originalBackground = this.el.style.backgroundColor;
+    this.el.style.color = 'white';
+    this.el.style.backgroundColor = 'black'
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.el.style.color = this.originalColor
+    this.el.style.backgroundColor = this.originalBackground;
+  }
+
+
 
   private el = inject(ElementRef).nativeElement as HTMLElement;
 
@@ -17,6 +38,9 @@ export class HightlightDirective implements OnInit {
 
   ngOnInit(): void {
     console.log('>>>>>>>>>>>>>>> laaaaa');
+    this.el.style.backgroundColor = this.background;
+    this.el.style.color = this.textColor;
+
     
     //this.el.style.backgroundColor = 'blue';
   }
